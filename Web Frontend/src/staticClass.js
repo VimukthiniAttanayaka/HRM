4f955955d4147eb3fe1af0ad0008ab7971290e9a
A.M.V.N.Attanayaka <73266263+VimukthiniAttanayaka@users.noreply.github.com>
2024-07-17 18:06:29 +0530

    add ststic file

---
+++ Web Frontend/src/staticClass.js
@@ -0,0 +1,14 @@
+// export const getRole = () => {
+//   return localStorage.getItem('role');
+// };
+
+export const getJWTToken = () => {
+  return localStorage.getItem('token');
+};
+export const getStaffID = () => {
+  return localStorage.getItem('staff_id');
+};
+
+export const getCustomerID = () => {
+  return localStorage.getItem('customer_id');
+};
