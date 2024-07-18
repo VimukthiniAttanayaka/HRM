be7f9a7e9898480a813f92f8800543f75b066c6f
A.M.V.N.Attanayaka <73266263+VimukthiniAttanayaka@users.noreply.github.com>
2024-07-18 14:45:29 +0530

    fvygubhij

--- Web Frontend/src/views/employee/employee/EmployeeDataGrid.js
+++ Web Frontend/src/views/employee/employee/EmployeeDataGrid.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { CCardBody, CButton, CSmartTable } from '@coreui/react-pro'
+import { CCardBody, CButton, CSmartTable, CBadge, CCollapse, } from '@coreui/react-pro'
 import { getJWTToken, getCustomerID, getStaffID } from '../../../staticClass.js';
 import data from './_data.js'
 const SmartTableDownloadableExample = () => {
@@ -31,7 +31,7 @@ const SmartTableDownloadableExample = () => {
       })
   }
   useEffect(() => {
-    requestdata();
+    // requestdata();
   }, []);
 
 
@@ -40,6 +40,36 @@ const SmartTableDownloadableExample = () => {
   const csvContent = currentItems.map((item) => Object.values(item).join(',')).join('\n')
 
   const csvCode = 'data:text/csv;charset=utf-8,SEP=,%0A' + encodeURIComponent(csvContent)
+
+
+  const [details, setDetails] = useState([])
+  const columns = [
+    {
+      key: 'name',
+      _style: { width: '40%' },
+    },
+    'registered',
+    { key: 'role', _style: { width: '20%' } },
+    { key: 'status', _style: { width: '10%' } },
+    { key: 'status', _style: { width: '10%' } },
+    {
+      key: 'show_details',
+      label: '',
+      _style: { width: '1%' },
+      filter: false,
+      sorter: false,
+    },
+  ]
+  const toggleDetails = (index) => {
+    const position = details.indexOf(index)
+    let newDetails = details.slice()
+    if (position !== -1) {
+      newDetails.splice(position, 1)
+    } else {
+      newDetails = [...details, index]
+    }
+    setDetails(newDetails)
+  }
 
   return (
     <CCardBody>
@@ -59,6 +89,41 @@ const SmartTableDownloadableExample = () => {
         itemsPerPage={5}
         onFilteredItemsChange={setCurrentItems}
         pagination
+        scopedColumns={{
+          show_details: (item) => {
+            return (
+              <td className="py-2">
+                <CButton
+                  color="primary"
+                  variant="outline"
+                  shape="square"
+                  size="sm"
+                  onClick={() => {
+                    toggleDetails(item.id)
+                  }}
+                >
+                  {details.includes(item.id) ? 'Hide' : 'Show'}
+                </CButton>
+              </td>
+            )
+          },
+          details: (item) => {
+            return (
+              <CCollapse visible={details.includes(item.id)}>
+                <CCardBody>
+                  <h4>{item.username}</h4>
+                  <p className="text-body-secondary">User since: {item.registered}</p>
+                  <CButton size="sm" color="info">
+                    User Settings
+                  </CButton>
+                  <CButton size="sm" color="danger" className="ml-1">
+                    Delete
+                  </CButton>
+                </CCardBody>
+              </CCollapse>
+            )
+          },
+        }}
       />
     </CCardBody>
   )
