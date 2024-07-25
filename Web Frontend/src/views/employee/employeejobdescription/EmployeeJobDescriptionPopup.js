a34450e8c5ad594f6b1a41b5730c3fce9d013f5a
Neelaka Karunasekara <neelaka.karunasekara@transnational-grp.com>
2024-07-25 10:12:40 +0530

    .

--- Web Frontend/src/views/employee/employeejobdescription/EmployeeJobDescriptionPopup.js
+++ Web Frontend/src/views/employee/employeejobdescription/EmployeeJobDescriptionPopup.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { CTooltip, CButton, CModal, CCol, CInputGroupText, CModalBody, CModalTitle, CModalFooter,CFormCheck,CModalHeader, CPopover, CLink, CCard, CCardBody, CForm, CFormInput, CInputGroup, CDatePicker } from '@coreui/react-pro'
+import { CTooltip, CButton, CModal, CCol, CInputGroupText, CModalBody, CModalTitle, CModalFooter, CFormCheck, CModalHeader, CPopover, CLink, CCard, CCardBody, CForm, CFormInput, CInputGroup, CDatePicker } from '@coreui/react-pro'
 import { getJWTToken, getCustomerID, getStaffID } from '../../../staticClass.js';
 import data from './_data.js'
 import { Modal } from '@coreui/coreui-pro';
@@ -27,21 +27,14 @@ const EmployeeJobDescriptionPopup = ({ visible, onClose, onOpen }) => {
           <CCard className="mx-4" md={9}>
             <CCardBody className="p-4">
               <CForm onSubmit={handleSubmit}>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>Company Id</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="Company Id" name="customerId"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
+
                 <CInputGroup className="mb-3">
                   <CCol md={4}>
                     <CInputGroupText>
                       <h6>Department Id</h6>
                     </CInputGroupText>
-                  </CCol> <CFormInput placeholder="Department Id" name="DepartmentId"
+                  </CCol> 
+                  <CFormInput placeholder="Department Id" name="DepartmentId"
                   // value={customerId} onChange={handleChangeId}
                   />
                 </CInputGroup>
@@ -58,126 +51,9 @@ const EmployeeJobDescriptionPopup = ({ visible, onClose, onOpen }) => {
                 <CInputGroup className="mb-3">
                   <CCol md={4}>
                     <CInputGroupText>
-                      <h6>FirstName</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="FirstName" name="FirstName"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>LastName</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="LastName" name="LastName"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>PrefferedName</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="PrefferedName" name="PrefferedName"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
                       <h6>JobTitle_Code</h6>
                     </CInputGroupText>
                   </CCol> <CFormInput placeholder="JobTitle_Code" name="JobTitle_Code"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>Address</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="Address" name="Address"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>EmailAddress</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="EmailAddress" name="EmailAddress"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>MobileNumber</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="MobileNumber" name="MobileNumber"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>PhoneNumber1</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="PhoneNumber1" name="PhoneNumber1"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>PhoneNumber2</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="PhoneNumber2" name="PhoneNumber2"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>RankDescription</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="RankDescription" name="RankDescription"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>StaffLocation</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="StaffLocation" name="StaffLocation"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>Remarks</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="Remarks" name="Remarks"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>LastResetDateTime</h6>
-                    </CInputGroupText>
-                  </CCol> <CFormInput placeholder="LastResetDateTime" name="LastResetDateTime"
-                  // value={customerId} onChange={handleChangeId}
-                  />
-                </CInputGroup>
-                <CInputGroup className="mb-3">
-                  <CCol md={4}>
-                    <CInputGroupText>
-                      <h6>SyncedDateTime</h6>
-                    </CInputGroupText>
-                  </CCol> <CDatePicker placeholder="SyncedDateTime" name="SyncedDateTime"
                   // value={customerId} onChange={handleChangeId}
                   />
                 </CInputGroup>
