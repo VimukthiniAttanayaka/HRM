2b73aa551a59e6ae279a0b5c8d44a47faf30ef09
Chirantha Poornajith <chiranthapoornajith@gmail.com>
2024-08-09 23:03:13 +0530

    data fetch from logged user db

--- Web Frontend/src/apicalls/employee/get_employee_single.js
+++ Web Frontend/src/apicalls/employee/get_employee_single.js
@@ -1,22 +1,51 @@
 const apiUrl = process.env.REACT_APP_API_URL;
 
+export class EmployeeDetail {
+  EME_CustomerID;
+  EME_DepartmentID;
+  EME_EmployeeID;
+  EME_FirstName;
+  EME_LastName;
+  EME_Gender;
+  EME_MaritalStatus;
+  EME_Nationality;
+  EME_BloodGroup;
+  EME_NIC;
+  EME_Passport;
+  EME_DrivingLicense;
+  EME_PrefferedName;
+  EME_JobTitle_Code;
+  EME_ReportingManager;
+  EME_EmployeeType;
+  EME_PayeeTaxNumber;
+  EME_Salary;
+  EME_Address;
+  EME_EmailAddress;
+  EME_MobileNumber;
+  EME_PhoneNumber1;
+  EME_PhoneNumber2;
+  EME_DateOfHire;
+  EME_Status;
+  EME_DateOfBirth;
+}
+// console.log(apiUrl)
 export const getEmployeeSingle = async (formData) => {
 
-  let resw;
+  let resw=new EmployeeDetail();
   const res = await fetch(apiUrl + 'employee/get_employee_single', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(formData),
   })
-    .then(response => response.json())
-    .then(json => {
-      let res1 = JSON.parse(JSON.stringify(json))
-      resw = res1[0].Employee[0]
-      // console.log(res2)
-      // console.log(res1[0].Employee[0])
-      // setLeaveTypeDetails(res1[0].LeaveType[0]);
-      // handleOpenPopup()
-    })
+  .then(response => response.json())
+  .then(json => {
+    let res1 = JSON.parse(JSON.stringify(json))
+    resw=res1[0].Employee[0]
+    // console.log(res2)
+    // console.log(res1[0].Employee[0])
+    // setLeaveTypeDetails(res1[0].LeaveType[0]);
+    // handleOpenPopup()
+  })
 
-  return resw;
+    return resw;
 };
--- Web Frontend/src/views/profile/ProfileTabContent.js
+++ Web Frontend/src/views/profile/ProfileTabContent.js
@@ -1,6 +1,7 @@
 import profile_img from '../../assets/images/avatars/7.jpg'
 import {CCol, CRow} from "@coreui/react-pro";
-function ProfileTabContent() {
+function ProfileTabContent({employeeData}) {
+
   return(
     <div>
       <CRow>
@@ -8,84 +9,84 @@ function ProfileTabContent() {
           <img src={profile_img} alt="profile image" className={'mt-3 rounded-circle'}/>
         </CCol>
         <CCol>
-          <h5 className={'mt-2'}>FirstName LastName <br/> Preferred Name</h5>
-          <p className={'mb-2'}>email@hmail.com</p>
-          <h4>Job Title</h4>
+          <h5 className={'mt-2'}>{employeeData.EME_FirstName} {employeeData.EME_LastName} <br/> {employeeData.EME_PrefferedName}</h5>
+          <p className={'mb-2'}>{employeeData.EME_EmailAddress}</p>
+          <h4>{employeeData.EME_PrefferedName}</h4>
           <p>
-            Employee Id : 012 <br/>
-            Department Id : S01
+            Employee Id : {employeeData.EME_EmployeeID} <br/>
+            Department Id : {employeeData.EME_DepartmentID}
           </p>
         </CCol>
       </CRow>
       <div className="">
         <CRow>
           <CCol className={'p-2 m-1'}>Reporting Manager :</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>name name</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_ReportingManager}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Status</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>Active</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_Status && 'Active'}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Employ Type</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>trainee</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_EmployeeType}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Payee Tax Number</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>tax num</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_PayeeTaxNumber}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Salary</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>LKR 550,000</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>LKR {employeeData.EME_Salary}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Date of Birth</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>1520/06/30</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_DateOfBirth}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Date of Hire</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>2023/03/15</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_DateOfHire}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Gender</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>female</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_Gender}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Marital Status</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>married</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_MaritalStatus}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Nationality</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>Sri Lankan</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_Nationality}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Blood Group</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>A-</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_BloodGroup}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>NIC</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>2134568988x</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_NIC}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Passport</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>null</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_Passport}</CCol>
         </CRow>
         <CRow>
           <CCol className={'p-2 m-1'}>Driving Licence</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>null</CCol>
+          <CCol className={'p-2 m-1 border rounded'}>{employeeData.EME_DrivingLicense}</CCol>
         </CRow>
-        <CRow>
-          <CCol className={'p-2 m-1'}>Created by</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>2023/11/12</CCol>
-        </CRow>
-        <CRow>
-          <CCol className={'p-2 m-1'}>Modified by</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>self</CCol>
-        </CRow>
-        <CRow>
-          <CCol className={'p-2 m-1'}>Modified date time</CCol>
-          <CCol className={'p-2 m-1 border rounded'}>2025/03/12 12:27:11</CCol>
-        </CRow>
+        {/*<CRow>*/}
+        {/*  <CCol className={'p-2 m-1'}>Created by</CCol>*/}
+        {/*  <CCol className={'p-2 m-1 border rounded'}>2023/11/12</CCol>*/}
+        {/*/!*</CRow>*!/*/}
+        {/*<CRow>*/}
+        {/*  <CCol className={'p-2 m-1'}>Modified by</CCol>*/}
+        {/*  <CCol className={'p-2 m-1 border rounded'}>self</CCol>*/}
+        {/*</CRow>*/}
+        {/*<CRow>*/}
+        {/*  <CCol className={'p-2 m-1'}>Modified date time</CCol>*/}
+        {/*  <CCol className={'p-2 m-1 border rounded'}>2025/03/12 12:27:11</CCol>*/}
+        {/*</CRow>*/}
       </div>
     </div>
   )
