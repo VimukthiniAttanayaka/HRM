1bcb250ea3f6e4dd7f891e291327d8b4f5d8353b
Neelaka Karunasekara <neelaka.karunasekara@transnational-grp.com>
2024-09-03 03:11:47 +0530

    .

--- Web Frontend/src/views/exitinterview/termination/Termination.js
+++ Web Frontend/src/views/exitinterview/termination/Termination.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import { CCard, CCardHeader, CCardBody } from '@coreui/react-pro'
-import TerminationDataGrid from './TerminationDataGrid'
+import { CCard, CCardHeader, CCardBody, CRow, CCol, CInputGroupText, CFormInput, CInputGroup, CButton } from '@coreui/react-pro'
 import { getLabelText } from 'src/MultipleLanguageSheets'
 
 const Termination = () => {
@@ -13,9 +12,84 @@ const Termination = () => {
           {getLabelText('Termination List', templatetype)}
         </CCardHeader>
         <CCardBody>
-          <TerminationDataGrid onOpen={() => handleOpenPopup} />
-        </CCardBody>
-      </CCard>
+          <CRow><CCol>
+            <CInputGroup className="mb-3">
+              <CCol md={4}>
+                <CInputGroupText>
+                  <h6>Employee ID</h6>
+                </CInputGroupText>
+              </CCol>
+              <CCol md={4}>
+                <CFormInput maxLength={20} placeholder="Employee ID" name="EmployeeID" />
+              </CCol>
+              <CCol md={4}>  <CButton color="primary"
+                className="mb-2">Search</CButton>
+              </CCol>   </CInputGroup>
+          </CCol></CRow>
+          <CRow><CCol>
+            <CInputGroup className="mb-3">
+              <CCol md={4}>
+                <CInputGroupText>
+                  <h6>NIC</h6>
+                </CInputGroupText>
+              </CCol>
+              <CCol md={4}>
+                <CFormInput readOnly maxLength={20} placeholder="NIC" name="NIC" />
+              </CCol>
+              <CCol md={4}>   </CCol>
+            </CInputGroup>
+          </CCol></CRow>
+          <CRow>
+            <CCol>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>Passport</h6>
+                  </CInputGroupText>
+                </CCol>
+                <CCol md={4}><CFormInput readOnly maxLength={20} placeholder="Passport" name="Passport" />
+                </CCol>
+                <CCol md={4}>   </CCol>
+              </CInputGroup>
+            </CCol></CRow>
+          <CRow>
+            <CCol>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>FirstName</h6>
+                  </CInputGroupText>
+                </CCol>
+                <CCol md={4}><CFormInput readOnly placeholder="FirstName" name="FirstName" />
+                </CCol>
+                <CCol md={4}>   </CCol>
+              </CInputGroup>
+            </CCol></CRow>
+          <CRow>
+            <CCol>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>LastName</h6>
+                  </CInputGroupText>
+                </CCol>
+                <CCol md={4}><CFormInput readOnly placeholder="LastName" name="LastName" />
+                </CCol>
+                <CCol md={4}>   </CCol>
+              </CInputGroup>
+            </CCol>
+          </CRow>
+          <CRow><CCol>
+            <CInputGroup className="mb-3">
+              <CCol md={4}>   </CCol>
+              <CCol md={4}>  <CButton color="primary"
+                className="mb-2">Confirm Termination</CButton></CCol>
+              <CCol md={4}>   </CCol>
+            </CInputGroup>
+          </CCol>
+          </CRow>
+        </CCardBody >
+      </CCard >
     </>
   )
 }
