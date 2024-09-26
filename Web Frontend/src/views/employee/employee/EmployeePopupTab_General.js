48c3906bc37fe75fe109c7ad7faa67cd18ac9298
Neelaka Karunasekara <neelaka.karunasekara@transnational-grp.com>
2024-09-26 16:37:37 +0530

    m

--- Web Frontend/src/views/employee/employee/EmployeePopupTab_General.js
+++ Web Frontend/src/views/employee/employee/EmployeePopupTab_General.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { CTooltip, CRow, CButton, CModal, CTabs, CFormSelect, CTabList, CTab, CCol, CInputGroupText, CTabContent, CTabPanel, CModalBody, CModalTitle, CModalFooter, CFormCheck, CModalHeader, CPopover, CLink, CCard, CCardBody, CForm, CFormInput, CInputGroup, CDatePicker } from '@coreui/react-pro'
+import { CTooltip, CRow, CButton, CModal, CTabs, CFormSelect, CCardHeader, CTabList, CTab, CCol, CInputGroupText, CTabContent, CTabPanel, CModalBody, CModalTitle, CModalFooter, CFormCheck, CModalHeader, CPopover, CLink, CCard, CCardBody, CForm, CFormInput, CInputGroup, CDatePicker } from '@coreui/react-pro'
 import { getJWTToken, getCustomerID, getStaffID } from '../../../staticClass.js';
 import axios from 'axios';
 import { modifyEmployee } from '../../../apicalls/employee/modify.js';
@@ -23,6 +23,11 @@ const EmployeePopupTab_General = ({ EmployeeDetails, popupStatus }) => {
   const [nic, setNic] = useState('')
   const [passport, setPassport] = useState('')
   const [drivingLicense, setDrivingLicense] = useState('')
+  const [Address, setAddress] = useState('')
+  const [EmailAddress, setEmailAddress] = useState('')
+  const [MobileNumber, setMobileNumber] = useState('')
+  const [PhoneNumber1, setPhoneNumber1] = useState('')
+  const [PhoneNumber2, setPhoneNumber2] = useState('')
 
   const handleChangeEmployeeId = (event) => {
     setEmployeeId(event.target.value)
@@ -60,6 +65,21 @@ const EmployeePopupTab_General = ({ EmployeeDetails, popupStatus }) => {
   const handleChangeDrivingLicenseNumber = (event) => {
     setDrivingLicense(event.target.value)
   }
+  const handleChangeAddress = (event) => {
+    setAddress(event.target.value)
+  }
+  const handleChangeEmailAddress = (event) => {
+    setEmailAddress(event.target.value)
+  }
+  const handleChangeMobileNumber = (event) => {
+    setMobileNumber(event.target.value)
+  }
+  const handleChangePhoneNumber1 = (event) => {
+    setPhoneNumber1(event.target.value)
+  }
+  const handleChangePhoneNumber2 = (event) => {
+    setPhoneNumber2(event.target.value)
+  }
 
   const handleSubmitData = async (event) => {
     event.preventDefault()
@@ -79,9 +99,15 @@ const EmployeePopupTab_General = ({ EmployeeDetails, popupStatus }) => {
       EME_NIC: nic,
       EME_Passport: passport,
       EME_DrivingLicense: drivingLicense,
+      EME_Address: Address,
+      EME_EmailAddress: EmailAddress,
+      EME_MobileNumber: MobileNumber,
+      EME_PhoneNumber1: PhoneNumber1,
+      EME_PhoneNumber2: PhoneNumber2
+
     }
 
- if (popupStatus == 'edit') {
+    if (popupStatus == 'edit') {
       const APIReturn = await modifyEmployee(formData)
       if (APIReturn.resp === false) { setDialogTitle("Alert"); }
       else { setDialogTitle("Message"); }
@@ -118,6 +144,11 @@ const EmployeePopupTab_General = ({ EmployeeDetails, popupStatus }) => {
       setNic('')
       setPassport('')
       setDrivingLicense('')
+      setAddress('')
+      setEmailAddress('')
+      setMobileNumber('')
+      setPhoneNumber1('')
+      setPhoneNumber2('')
     }
     else {
       const createdDOB = EmployeeDetails.EME_DateOfBirth;
@@ -136,6 +167,11 @@ const EmployeePopupTab_General = ({ EmployeeDetails, popupStatus }) => {
       setNic(EmployeeDetails.EME_NIC)
       setPassport(EmployeeDetails.EME_Passport)
       setDrivingLicense(EmployeeDetails.EME_DrivingLicense)
+      setAddress(EmployeeDetails.EME_Address)
+      setEmailAddress(EmployeeDetails.EME_EmailAddress)
+      setMobileNumber(EmployeeDetails.EME_MobileNumber)
+      setPhoneNumber1(EmployeeDetails.EME_PhoneNumber1)
+      setPhoneNumber2(EmployeeDetails.EME_PhoneNumber2)
     }
   }, [EmployeeDetails]);
 
@@ -170,74 +206,8 @@ const EmployeePopupTab_General = ({ EmployeeDetails, popupStatus }) => {
               value={lastName} onChange={handleChangeLastName}
               disabled={(popupStatus == 'view' || popupStatus == 'delete') ? true : false} />
           </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CCol md={4}>
-              <CInputGroupText>
-                <h6>Date of birth</h6>
-              </CInputGroupText>
-            </CCol>
-            <input
-              type="date"
-              id="dob"
-              name="dob"
-              value={dob}
-              onChange={handleChangeDob}
-            />
-          </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CCol md={4}>
-              <CInputGroupText>
-                <h6>Gender</h6>
-              </CInputGroupText>
-            </CCol>
-            <CFormCheck inline type="radio" name="gender" id="inlineCheckbox1" value="male" label="Male" checked={gender === 'male'}
-              onChange={handleChangeGender} className='ms-4' />
-            <CFormCheck inline type="radio" name="gender" id="inlineCheckbox2" value="female" label="Female" checked={gender === 'female'}
-              onChange={handleChangeGender} />
-          </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CCol md={4}>
-              <CInputGroupText>
-                <h6>Marital status</h6>
-              </CInputGroupText>
-            </CCol>
-            <CFormCheck inline type="radio" name="maritalStatus" id="inlineCheckbox1" value="singal" label="Single" checked={maritalStatus === 'single'}
-              onChange={handleChangeMaritalStatus} className='ms-4' />
-            <CFormCheck inline type="radio" name="maritalStatus" id="inlineCheckbox2" value="married" label="Married" checked={maritalStatus === 'married'}
-              onChange={handleChangeMaritalStatus} />
-            <CFormCheck inline type="radio" name="maritalStatus" id="inlineCheckbox2" value="widowed" label="Widowed" checked={maritalStatus === 'widowed'}
-              onChange={handleChangeMaritalStatus} />
-            <CFormCheck inline type="radio" name="maritalStatus" id="inlineCheckbox2" value="divorced" label="Divorced" checked={maritalStatus === 'divorced'}
-              onChange={handleChangeMaritalStatus} />
-          </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CCol md={4}>
-              <CInputGroupText>
-                <h6>Nationality/Country</h6>
-              </CInputGroupText>
-            </CCol>
-            <CFormSelect value={nationality} onChange={handleChangeNationality}>
-              <option value="srilankan">Srilankan</option>
-              <option value="indian">Indian</option>
-            </CFormSelect>
-          </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CCol md={4}>
-              <CInputGroupText>
-                <h6>Blood group</h6>
-              </CInputGroupText>
-            </CCol>
-            <CFormSelect value={bloodGroup} onChange={handleChangeBloodGroup}>
-              <option value="o+">O+</option>
-              <option value="o-">O-</option>
-              <option value="a+">A+</option>
-              <option value="a-">A-</option>
-              <option value="b+">B+</option>
-              <option value="b-">B-</option>
-              <option value="ab+">AB+</option>
-              <option value="ab-">AB-</option>
-            </CFormSelect>
-          </CInputGroup>
+
+
           <CInputGroup className="mb-3">
             <CCol md={4}>
               <CInputGroupText>
@@ -247,39 +217,166 @@ const EmployeePopupTab_General = ({ EmployeeDetails, popupStatus }) => {
               value={prefferedName} onChange={handleChangePrefferedName}
               disabled={(popupStatus == 'view' || popupStatus == 'delete') ? true : false} />
           </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CCol md={4}>
-              <CInputGroupText>
-                <h6>NIC</h6>
-              </CInputGroupText>
-            </CCol> <CFormInput placeholder="NIC" name="nic"
-              value={nic} onChange={handleChangeNicNumber}
-              disabled={(popupStatus == 'view' || popupStatus == 'delete') ? true : false} />
-          </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CCol md={4}>
-              <CInputGroupText>
-                <h6>Passport</h6>
-              </CInputGroupText>
-            </CCol> <CFormInput placeholder="Passport Number" name="passportNumber"
-              value={passport} onChange={handleChangePassportNumber}
-              disabled={(popupStatus == 'view' || popupStatus == 'delete') ? true : false} />
-          </CInputGroup>
-          <CInputGroup className="mb-3">
-            <CCol md={4}>
-              <CInputGroupText>
-                <h6>Driving License</h6>
-              </CInputGroupText>
-            </CCol> <CFormInput placeholder="Driving License" name="drivingLicense"
-              value={drivingLicense} onChange={handleChangeDrivingLicenseNumber}
-              disabled={(popupStatus == 'view' || popupStatus == 'delete') ? true : false} />
-          </CInputGroup>
+
+          <CCard>
+            <CCardHeader>
+              Civil Status
+            </CCardHeader>
+            <CCardBody>
+
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>Gender</h6>
+                  </CInputGroupText>
+                </CCol>
+                <CFormCheck inline type="radio" name="gender" id="inlineCheckbox1" value="male" label="Male" checked={gender === 'male'}
+                  onChange={handleChangeGender} className='ms-4' />
+                <CFormCheck inline type="radio" name="gender" id="inlineCheckbox2" value="female" label="Female" checked={gender === 'female'}
+                  onChange={handleChangeGender} />
+              </CInputGroup>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>Marital status</h6>
+                  </CInputGroupText>
+                </CCol>
+                <CFormCheck inline type="radio" name="maritalStatus" id="inlineCheckbox1" value="singal" label="Single" checked={maritalStatus === 'single'}
+                  onChange={handleChangeMaritalStatus} className='ms-4' />
+                <CFormCheck inline type="radio" name="maritalStatus" id="inlineCheckbox2" value="married" label="Married" checked={maritalStatus === 'married'}
+                  onChange={handleChangeMaritalStatus} />
+                <CFormCheck inline type="radio" name="maritalStatus" id="inlineCheckbox2" value="widowed" label="Widowed" checked={maritalStatus === 'widowed'}
+                  onChange={handleChangeMaritalStatus} />
+                <CFormCheck inline type="radio" name="maritalStatus" id="inlineCheckbox2" value="divorced" label="Divorced" checked={maritalStatus === 'divorced'}
+                  onChange={handleChangeMaritalStatus} />
+              </CInputGroup>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>Nationality/Country</h6>
+                  </CInputGroupText>
+                </CCol>
+                <CFormSelect value={nationality} onChange={handleChangeNationality}>
+                  <option value="srilankan">Srilankan</option>
+                  <option value="indian">Indian</option>
+                </CFormSelect>
+              </CInputGroup>
+              <CInputGroup className="mb-3">
+                <CCol md={2}>
+                  <CInputGroupText>
+                    <h6>Date of birth</h6>
+                  </CInputGroupText>
+                </CCol>
+                <CCol md={5}><input
+                  type="date"
+                  id="dob"
+                  name="dob"
+                  value={dob}
+                  onChange={handleChangeDob}
+                /> </CCol>
+                <CCol md={3}>
+                  <CInputGroupText>
+                    <h6>Blood group</h6>
+                  </CInputGroupText>
+                </CCol>
+                <CFormSelect value={bloodGroup} onChange={handleChangeBloodGroup}>
+                  <option value="o+">O+</option>
+                  <option value="o-">O-</option>
+                  <option value="a+">A+</option>
+                  <option value="a-">A-</option>
+                  <option value="b+">B+</option>
+                  <option value="b-">B-</option>
+                  <option value="ab+">AB+</option>
+                  <option value="ab-">AB-</option>
+                </CFormSelect>
+              </CInputGroup>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>NIC</h6>
+                  </CInputGroupText>
+                </CCol> <CFormInput placeholder="NIC" name="nic"
+                  value={nic} onChange={handleChangeNicNumber}
+                  disabled={(popupStatus == 'view' || popupStatus == 'delete') ? true : false} />
+              </CInputGroup>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>Passport</h6>
+                  </CInputGroupText>
+                </CCol> <CFormInput placeholder="Passport Number" name="passportNumber"
+                  value={passport} onChange={handleChangePassportNumber}
+                  disabled={(popupStatus == 'view' || popupStatus == 'delete') ? true : false} />
+              </CInputGroup>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>Driving License</h6>
+                  </CInputGroupText>
+                </CCol> <CFormInput placeholder="Driving License" name="drivingLicense"
+                  value={drivingLicense} onChange={handleChangeDrivingLicenseNumber}
+                  disabled={(popupStatus == 'view' || popupStatus == 'delete') ? true : false} />
+              </CInputGroup>
+            </CCardBody>
+          </CCard>
+          <CCard>
+            <CCardHeader>
+              General Address
+            </CCardHeader>
+            <CCardBody>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>Address</h6>
+                  </CInputGroupText>
+                </CCol> <CFormInput placeholder="Address" name="Address"
+                  value={Address} onChange={handleChangeAddress}
+                />
+              </CInputGroup>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>EmailAddress</h6>
+                  </CInputGroupText>
+                </CCol> <CFormInput placeholder="EmailAddress" name="emailAddress"
+                  value={EmailAddress} onChange={handleChangeEmailAddress}
+                />
+              </CInputGroup>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>MobileNumber</h6>
+                  </CInputGroupText>
+                </CCol> <CFormInput placeholder="MobileNumber" name="mobileNumber"
+                  value={MobileNumber} onChange={handleChangeMobileNumber}
+                />
+              </CInputGroup>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>PhoneNumber1</h6>
+                  </CInputGroupText>
+                </CCol> <CFormInput placeholder="PhoneNumber1" name="phoneNumber1"
+                  value={PhoneNumber1} onChange={handleChangePhoneNumber1}
+                />
+              </CInputGroup>
+              <CInputGroup className="mb-3">
+                <CCol md={4}>
+                  <CInputGroupText>
+                    <h6>PhoneNumber2</h6>
+                  </CInputGroupText>
+                </CCol> <CFormInput placeholder="PhoneNumber2" name="phoneNumber2"
+                  value={PhoneNumber2} onChange={handleChangePhoneNumber2}
+                />
+              </CInputGroup>
+            </CCardBody>
+          </CCard>
           <div className="d-grid">
             {popupStatus == 'view' ? '' : (popupStatus == 'delete' ? <CButton color="danger" type='submit'>Delete</CButton> :
               <CButton color="success" type='submit'>Submit</CButton>)}
           </div>
         </CForm>
-      </CTabPanel>
+      </CTabPanel >
     </>
   )
 }
