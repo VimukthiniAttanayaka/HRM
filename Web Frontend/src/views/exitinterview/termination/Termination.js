7a42a459f1796f28f415f35fa0910f0caf7889e6
Neelaka Karunasekara <neelaka.karunasekara@transnational-grp.com>
2024-09-30 03:20:56 +0530

    .

--- Web Frontend/src/views/exitinterview/termination/Termination.js
+++ Web Frontend/src/views/exitinterview/termination/Termination.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { CCard, CCardHeader, CCardBody, CRow, CCol, CInputGroupText, CFormInput, CInputGroup, CButton } from '@coreui/react-pro'
+import { getJWTToken, getCustomerID, getStaffID } from '../../../staticClass.js';
 import { getLabelText } from 'src/MultipleLanguageSheets'
+import { getEmployeeSingle } from '../../../apicalls/employee/get_employee_single.js';
 
 const Termination = () => {
   let templatetype = 'translation_Termination'
+  let templatetype_base = 'translation'
+
+  const [EmployeeID, setEmployeeID] = useState('')
+  const [NIC, setNIC] = useState('')
+  const [Passport, setPassport] = useState('')
+  const [LastName, setLastName] = useState('')
+  const [FirstName, setFirstName] = useState('')
+  const handleChangeEmployeeID = (event) => { setEmployeeID(event.target.value) }
+
+  async function loadDetails(item) {
+
+    const token = getJWTToken();
+    const staffId = getStaffID();
+    const customerId = getCustomerID();
+
+    const formData = {
+      // UD_StaffID: staffId,
+      // AUD_notificationToken: token,
+      EME_EmployeeID: EmployeeID
+    }
+
+    const EmployeeDetails = await getEmployeeSingle(formData)
+    console.log(EmployeeDetails)
+    setNIC(EmployeeDetails.EME_NIC)
+    setPassport(EmployeeDetails.EME_Passport)
+    setLastName(EmployeeDetails.EME_LastName)
+    setFirstName(EmployeeDetails.EME_FirstName)
+  }
+  
+  async function confirmTermination(item) {
+
+    const token = getJWTToken();
+    const staffId = getStaffID();
+    const customerId = getCustomerID();
+
+    const formData = {
+      // UD_StaffID: staffId,
+      // AUD_notificationToken: token,
+      EME_EmployeeID: EmployeeID
+    }
+  }
+
   return (
     <>
       <CCard className="mb-4">
@@ -20,11 +64,14 @@ const Termination = () => {
                 </CInputGroupText>
               </CCol>
               <CCol md={4}>
-                <CFormInput maxLength={20} placeholder="Employee ID" name="EmployeeID" />
+                <CFormInput maxLength={20} placeholder="Employee ID" name="EmployeeID" value={EmployeeID} onChange={handleChangeEmployeeID} />
               </CCol>
               <CCol md={4}>
                 <CButton color="primary"
-                  className="mb-2">Search</CButton>
+                  className="mb-2"
+                  onClick={() => {
+                    loadDetails(EmployeeID)
+                  }}>Search</CButton>
               </CCol>   </CInputGroup>
           </CCol></CRow>
           <CRow><CCol>
@@ -35,7 +82,7 @@ const Termination = () => {
                 </CInputGroupText>
               </CCol>
               <CCol md={4}>
-                <CFormInput readOnly maxLength={20} placeholder="NIC" name="NIC" />
+                <CFormInput readOnly maxLength={20} placeholder="NIC" name="NIC" value={NIC} />
               </CCol>
               <CCol md={4}>   </CCol>
             </CInputGroup>
@@ -48,7 +95,7 @@ const Termination = () => {
                     <h6>Passport</h6>
                   </CInputGroupText>
                 </CCol>
-                <CCol md={4}><CFormInput readOnly maxLength={20} placeholder="Passport" name="Passport" />
+                <CCol md={4}><CFormInput readOnly maxLength={20} placeholder="Passport" name="Passport" value={Passport} />
                 </CCol>
                 <CCol md={4}>   </CCol>
               </CInputGroup>
@@ -61,7 +108,7 @@ const Termination = () => {
                     <h6>FirstName</h6>
                   </CInputGroupText>
                 </CCol>
-                <CCol md={4}><CFormInput readOnly placeholder="FirstName" name="FirstName" />
+                <CCol md={4}><CFormInput readOnly placeholder="FirstName" name="FirstName" value={FirstName} />
                 </CCol>
                 <CCol md={4}>   </CCol>
               </CInputGroup>
@@ -74,7 +121,7 @@ const Termination = () => {
                     <h6>LastName</h6>
                   </CInputGroupText>
                 </CCol>
-                <CCol md={4}><CFormInput readOnly placeholder="LastName" name="LastName" />
+                <CCol md={4}><CFormInput readOnly placeholder="LastName" name="LastName" value={LastName} />
                 </CCol>
                 <CCol md={4}>   </CCol>
               </CInputGroup>
@@ -83,8 +130,11 @@ const Termination = () => {
           <CRow><CCol>
             <CInputGroup className="mb-3">
               <CCol md={4}>   </CCol>
-              <CCol md={4}>  <CButton color="primary"
-                className="mb-2">Confirm Termination</CButton></CCol>
+              <CCol md={4}>
+                <CButton color="primary"
+                  className="mb-2" onClick={() => {
+                    confirmTermination()
+                  }}>Confirm Termination</CButton></CCol>
               <CCol md={4}>   </CCol>
             </CInputGroup>
           </CCol>
