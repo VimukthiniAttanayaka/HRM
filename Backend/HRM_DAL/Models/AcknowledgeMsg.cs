4adc6475b054421e29010125d517f4115314a763
Neelaka Karunasekara <neelaka.karunasekara@transnational-grp.com>
2024-10-02 01:09:06 +0530

    .

--- Backend/HRM_DAL/Models/AcknowledgeMsg.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HRM_DAL.Models
-{
-    public class AcknowledgeMsg
-    {
-        public int messageId { get; set; }
-        public string message { get; set; }
-        public ResultOAuthToken result { get; set; }
-    }
-}
--- Backend/HRM_DAL/Models/UserGroupDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace HRM_DAL.Models
-{
-    public class UserGroupDto
-    {
-        public int messageId { get; set; }
-        public string message { get; set; }
-        public UserGroupsResult result { get; set; }
-    }
-}
